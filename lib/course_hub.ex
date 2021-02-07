673b7682a6e607fd565d9b0671d56e7dc8054d36
Aram Sperduti Menocci <aramsm3@gmail.com>
2021-02-06 22:11:51 -0300

    [course_hub] configured schema macro

--- lib/course_hub.ex
+++ lib/course_hub.ex
@@ -6,4 +6,35 @@ defmodule CourseHub do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  def schema do
+    quote do
+      alias CourseHub.Repo
+      alias Ecto.Changeset
+
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+      @timestamps_opts [type: :naive_datetime_usec]
+
+      # Specs
+
+      @type changeset_data ::
+              Ecto.Schema.t()
+              | Ecto.Changeset.t()
+              | {Ecto.Changeset.data(), Ecto.Changeset.types()}
+      @type changeset_params :: %{binary => term} | %{atom => term} | :invalid
+    end
+  end
+
+  @doc """
+  When used, dispatch to the appropriate controller/view/etc.
+  """
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
 end
