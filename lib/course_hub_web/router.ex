7ec1c136a182ac9912caf68ece8718ecd67951ac
Aram Sperduti Menocci <aramsm3@gmail.com>
2021-02-10 14:07:18 -0300

    [router] created all routes

--- lib/course_hub_web/router.ex
+++ lib/course_hub_web/router.ex
@@ -5,9 +5,21 @@ defmodule CourseHubWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :authenticated_account do
+    plug :accepts, ["json"]
+    plug CourseHubWeb.AuthenticatePipeline
+  end
+
   scope "/api/v1", CourseHubWeb do
     pipe_through :api
 
     post("/sign_in", Accounts.AccountController, :sign_in)
+  end
+
+  scope "/api/v1", CourseHubWeb do
+    pipe_through :authenticated_account
+
+    get("/courses", Universities.CourseController, :get_courses)
+    get("/offers", Financials.OfferController, :get_offers)
   end
 end
